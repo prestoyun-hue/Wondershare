a6c18c1c16babb62ebe157ec7835f32242882a26
unknown <prestoyun@estc.co.kr>
2026-01-20 09:14:36 +0900

    Fix: remove importmap and adjust build chunk limit

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // 500KB -> 1000KB로 경고 기준 상향
+  },
 });
